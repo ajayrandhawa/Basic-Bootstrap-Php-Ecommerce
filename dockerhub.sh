7406e282ce5b717b95b0fd33640fb0a7b5e99ab8
RAJA9658 <rajatpanda472@gmail.com>
2022-11-16 15:27:52 +0530

    Update dockerhub.sh

--- dockerhub.sh
+++ dockerhub.sh
@@ -1,4 +1,4 @@
 TOKEN=	curl -s -H "Content-Type: application/json" -X POST -H "Content-Type: application/json" -d '{"username":"raja9658" , "password":Raja@12345}' "https://hub.docker.com/v2/users/login/"
 curl "https://hub.docker.com/v2/repositories/raja9658/docker-version/tags/" -X GET -H "Authorization: JWT ${TOKEN}"
 
-curl "https://hub.docker.com/v2/repositories/raja9658/docker-version/tags/3/" -X DELETE -H "Authorization: JWT ${TOKEN}"
+curl "https://hub.docker.com/v2/repositories/raja9658/docker-version/tags/6/" -X DELETE -H "Authorization: JWT ${TOKEN}"
