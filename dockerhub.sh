f66d429e16f895eb35ed9c0341548d4816f6f085
RAJA9658 <rajatpanda472@gmail.com>
2022-11-28 12:40:11 +0530

    Update dockerhub.sh

--- dockerhub.sh
+++ dockerhub.sh
@@ -19,7 +19,7 @@ echo $IMAGE_TAGS
 FIRST_TAG=$(curl -s -H "Authorization: JWT ${TOKEN}" https://hub.docker.com/v2/repositories/${ORGANIZATIONNAME}/${REPOSITORY}/tags/  | jq -r '.results|.[7]|.name')
 echo "first tag is ${FIRST_TAG}"
 ## Please uncomment below line to delete docker hub images of docker hub repositories 
-##curl -s  -X DELETE  -H "Authorization: JWT ${TOKEN}" https://hub.docker.com/v2/repositories/${ORGANIZATIONNAME}/${REPOSITORY}/tags/${FIRST_TAG}/
+curl -s  -X DELETE  -H "Authorization: JWT ${TOKEN}" https://hub.docker.com/v2/repositories/${ORGANIZATIONNAME}/${REPOSITORY}/tags/${FIRST_TAG}/
 echo "$?"
 
 if [ ${FIRST_TAG} = "null" ]
